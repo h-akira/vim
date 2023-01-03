93432f5c1eacf28a83a7b962895a2376c08cea72
h-akira <79441679+h-akira@users.noreply.github.com>
2023-01-03 17:43:52 +0900

    template

--- template/template.py
+++ template/template.py
@@ -3,6 +3,11 @@
 #
 # Created: <+DATE+>
 
+# Import
+import sys
+import os
+import numpy
+
 def parse_args():
   import argparse
   parser = argparse.ArgumentParser(description="""\
@@ -13,22 +18,21 @@ def parse_args():
   parser.add_argument("-l", "--little", action="store_true", help="little endian")
   parser.add_argument("file", metavar="input-file", help="input file")
   options = parser.parse_args()
-  return options
-
-def main(options):
-  # Import
-  import sys
-  import os
-  import numpy
   
   # Initial Read
-  if(not os.path.isfile(options.file)):
-    raise Exception("The input file does not exist.")
-  global end
+  if(not os.path.isfile(options.file)): 
+    raise Exception("The input file does not exist.") 
+  global end 
   end = ">"
-  if(options.little): end = "<"
+  if(options.little):
+    end = "<"
+
+  return options
+
+def main():
+  # ArgumentParser
+  options = parse_args()
 
 
 if(__name__ == '__main__'):
-  options = parse_args()
-  main(options)
+  main()
